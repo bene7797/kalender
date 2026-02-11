bdf12b4bbe2cb2fe0ec56c60dc23750c2b327ede
bene7797 <benedikt.stoeck@web.de>
2026-02-11 16:54:48 +0100

    bilder hinzugefügt

---
+++ Images.js
@@ -0,0 +1,10 @@
+export class Images{
+
+
+    constructor(){
+        this.winterImages = ["Bilder/Winter01.jpg","Bilder/Winter02.jpg","Bilder/Winter03.jpg","Bilder/Winter04.jpg"];
+        this.sommerImages = ["Bilder/Sommer01.jpg","Bilder/Sommer02.jpg"];
+        this.fruehlingImages = ["Bilder/Fruehling01.jpg","Bilder/Fruehling02.jpg","Bilder/Fruehling.jpg","Bilder/Fruehling.jpg"];
+        this.herbstImages = ["Bilder/Herbst01.jpg","Bilder/Herbst02.jpg","Bilder/Herbst03.jpg"];
+    }
+}
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { Calender } from "./Calendar.js";
 import { ParticleCanvas } from "./ParticleCanvas.js";
+import { Images } from "./Images.js";
 
 
 let meinKalender = new Calender(new Date(2026,3,10));
@@ -10,17 +11,16 @@ const kalendar = document.getElementById("Kalender");
 const historicInfo = document.getElementById("historical");
 const nextMonthButton = document.getElementById("nextMonth");
 const previousMonthButton = document.getElementById("previousMonth");
+const myImages = new Images();
+const meinBody = document.body;
+//meinBody.style.backgroundImage ="/Bilder/Winter01.jpg";
 
 meinKalender.initTableEvents(kalendar);
-
-
 
 const pc = new ParticleCanvas();
 pc.initSnow();
 pc.animate();
 window.addEventListener('resize', () => { pc.resize();});
-
-
 
 previousMonthButton.addEventListener(`click`, () => {
   meinKalender.previousMonth();
@@ -34,10 +34,6 @@ datum.innerHTML = meinKalender.getDateString();
 info.innerHTML = meinKalender.getDayInfoText();
 kalendar.innerHTML = meinKalender.getCalendarHTML();
 historicInfo.innerHTML = await meinKalender.anzeigen();
-
-//meinCanvas.draw();
-
-
 
 window.addEventListener(`calendarUpdate`,async () => {
     console.log("Datum geändert");
